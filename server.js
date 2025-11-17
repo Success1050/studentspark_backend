bf8fcde2e1c218e9ad4931155d4dd6b735c6ddf4
emmanuel success <emmanuelekwunife58@gmail.com>
2025-11-17 05:59:12 +0100

    added pollufill

--- server.js
+++ server.js
@@ -8,6 +8,47 @@ import { generatequiz } from "./AIRoutes/uploadNotesForQuizGeneration.js";
 import { motivationGen } from "./AIRoutes/motivationGen.js";
 import { paystackPayment } from "./AIRoutes/paystackRoute.js";
 
+// import { createCanvas } from "canvas";
+
+global.DOMMatrix = class DOMMatrix {
+  constructor(init) {
+    this.a = 1;
+    this.b = 0;
+    this.c = 0;
+    this.d = 1;
+    this.e = 0;
+    this.f = 0;
+    this.m11 = 1;
+    this.m12 = 0;
+    this.m13 = 0;
+    this.m14 = 0;
+    this.m21 = 0;
+    this.m22 = 1;
+    this.m23 = 0;
+    this.m24 = 0;
+    this.m31 = 0;
+    this.m32 = 0;
+    this.m33 = 1;
+    this.m34 = 0;
+    this.m41 = 0;
+    this.m42 = 0;
+    this.m43 = 0;
+    this.m44 = 1;
+  }
+  translate(x, y, z) {
+    return this;
+  }
+  scale(x, y, z) {
+    return this;
+  }
+  rotate(angle) {
+    return this;
+  }
+  multiply(other) {
+    return this;
+  }
+};
+
 dotenv.config();
 
 const app = express();
